abf27621c61c6490029b56997bbd6f6ac348bf8c
Kamal.ELMAGOURI <kamalelmagouri@gmail.com>
2025-08-28 14:49:51 +0100

    edit build.sh by remove unused libraries

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 pip3 install --upgrade pysqlite3-binary fastapi uvicorn
 pip3 install --upgrade langchain
 pip3 install --upgrade langchain_google_genai langchain
-pip3 install --upgrade google-generativeai
+# pip3 install --upgrade google-generativeai
 pip3 install --upgrade langchain-community pydantic langchain-huggingface
-pip3 install --upgrade langchain_deepseek   
+# pip3 install --upgrade langchain_deepseek   
 
